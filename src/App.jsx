e3d283b261da909060ce2118e38bfdec21424b14
Alfredo Ajalla <aajalla@protranslating.com>
2024-11-05 08:39:08 -0500

    Add redux-devtools-extension package for debugging purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { Provider } from 'react-redux'
 import './App.css'
+import store from './redux/store'
 
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
       <h1 className="text-5xl font-bold">
         Hello from Buenos Aires
       </h1>
-    </>
+    </Provider>
   )
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import todoReducers from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(todoReducers);
+const store = createStore(
+  todoReducers,
+  composeWithDevTools());
 
 export default store;
